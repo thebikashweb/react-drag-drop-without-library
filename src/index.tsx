9fec4be1c0033712fde4ef691020c3f1eaa2de36
aleksey <alekseykaydash@gmail.com>
2023-03-10 00:41:12 +0300

    add coment hello

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
-{/* Hello World */}
+{/* Hello */}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
