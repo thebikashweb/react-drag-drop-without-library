8027b4f031efd3f406970d35f4ed69e51bdf74e0
aleksey <alekseykaydash@gmail.com>
2023-03-10 00:35:53 +0300

    add coment hello world

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
+{/* Hello World */}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
 	<React.StrictMode>
