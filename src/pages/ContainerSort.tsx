16e19664dfa5fd0bcbb6ead1e4e90aac223d757d
Bikash <bikashxsharma@gmail.com>
2023-05-28 22:39:27 +0300

    Container drag with multiple columns

--- src/pages/ContainerSort.tsx
+++ src/pages/ContainerSort.tsx
@@ -4,37 +4,70 @@ import { v4 as uuidv4 } from "uuid";
 type TodoType = {
   id: string;
   title: string;
-  isCompleted: boolean;
+  column: ColumnType;
   sortIndex: number;
 };
 
+const sampleTodos: Array<TodoType> = [
+  {
+    id: uuidv4(),
+    title: "Create a video ",
+    column: "incomplete",
+    sortIndex: 1,
+  },
+  {
+    id: uuidv4(),
+    title: "Edit video",
+    column: "incomplete",
+    sortIndex: 2,
+  },
+  {
+    id: uuidv4(),
+    title: "Publish video ",
+    column: "incomplete",
+    sortIndex: 3,
+  },
+];
+
+const columns = {
+  incomplete: true,
+  progress: true,
+
+  completed: true,
+  another: true,
+};
+
+type Column = typeof columns;
+
+type ColumnType = keyof Column;
+
 function ContainerSort() {
   const [todoTitle, setTodoTitle] = React.useState("");
-  const [todos, setTodos] = React.useState<TodoType[]>([]);
+  const [todos, setTodos] = React.useState<TodoType[]>(sampleTodos);
 
   const draggedTodoItem = React.useRef<any>(null);
 
   const handleAddTodo = () => {
     if (!todoTitle) return;
-    const todoPayload = {
+    const todoPayload: TodoType = {
       id: uuidv4(),
       title: todoTitle,
-      isCompleted: false,
+      column: "incomplete",
       sortIndex: todos.length + 1,
     };
 
     setTodos((prev) => [...prev, todoPayload]);
   };
 
-  //handle drag on container
-  const handleContainerDrag = (container: "incomplete" | "completed") => {
+  const handleContainerDrag = (column: ColumnType) => {
     const index = todos.findIndex(
       (todo) => todo.id === draggedTodoItem.current
     );
-    const tempTodos = todos;
-    tempTodos[index].isCompleted = container === "completed" ? true : false;
+    const tempTodos = [...todos];
+    tempTodos[index].column = column;
     setTodos([...tempTodos]);
   };
+
   return (
     <div className="container-sort">
       <div className="input-group">
@@ -51,52 +84,31 @@ function ContainerSort() {
       </div>
 
       <div className="container-sort__wrapper">
-        <div className="container-sort__incomplete">
-          <h5>Incomplete todos</h5>
-          <div
-            className="container-sort__items"
-            onDragOver={(e) => e.preventDefault()}
-            onDrop={() => handleContainerDrag("incomplete")}
-          >
-            {todos
-              .filter((todo) => !todo.isCompleted)
-              .map((todo) => (
-                <div
-                  key={todo.id}
-                  className="list-item"
-                  draggable
-                  onDragOver={(e) => e.preventDefault()}
-                  onDragStart={() => (draggedTodoItem.current = todo.id)}
-                >
-                  <i className="fa-solid fa-bars"></i>
-                  <h6>{todo.title}</h6>
-                </div>
-              ))}
+        {Object.keys(columns).map((key: any) => (
+          <div key={key} className={`container-sort__column`}>
+            <h5>{key.toLocaleUpperCase()} todos</h5>
+            <div
+              className="container-sort__items"
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={() => handleContainerDrag(key)}
+            >
+              {todos
+                .filter((todo) => todo.column === key)
+                .map((todo) => (
+                  <div
+                    key={todo.id}
+                    className="list-item"
+                    draggable
+                    onDragStart={(e) => (draggedTodoItem.current = todo.id)}
+                    onDragOver={(e) => e.preventDefault()}
+                  >
+                    <i className="fa-solid fa-bars"></i>
+                    <h6>{todo.title}</h6>
+                  </div>
+                ))}
+            </div>
           </div>
-        </div>
-        <div className="container-sort__completed">
-          <h5>Completed todos</h5>
-          <div
-            className="container-sort__items"
-            onDragOver={(e) => e.preventDefault()}
-            onDrop={() => handleContainerDrag("completed")}
-          >
-            {todos
-              .filter((todo) => todo.isCompleted)
-              .map((todo) => (
-                <div
-                  key={todo.id}
-                  className="list-item"
-                  draggable
-                  onDragOver={(e) => e.preventDefault()}
-                  onDragStart={() => (draggedTodoItem.current = todo.id)}
-                >
-                  <i className="fa-solid fa-bars"></i>
-                  <h6>{todo.title}</h6>
-                </div>
-              ))}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
